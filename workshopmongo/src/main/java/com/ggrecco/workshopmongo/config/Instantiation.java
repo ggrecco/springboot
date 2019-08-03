8dffabfd38e1a75fc1525eb7e5737a54f4ecd7f0
Gustavo Grecco <gustavo.grecco@hotmail.com>
2019-08-03 16:10:34 -0300

    instanciando base de dados

---
+++ workshopmongo/src/main/java/com/ggrecco/workshopmongo/config/Instantiation.java
@@ -0,0 +1,31 @@
+package com.ggrecco.workshopmongo.config;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.ggrecco.workshopmongo.domain.User;
+import com.ggrecco.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		// deleta toda a coleção que estiver presente no banco
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}
